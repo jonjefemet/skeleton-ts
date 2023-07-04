67789d1b9a41c13db2aaacd8373dc47072212104
jonjefemet <jonshua_97@outlook.com>
2023-07-03 20:28:25 -0600

    fix

--- src/middleware/authenticateToken.ts
+++ src/middleware/authenticateToken.ts
@@ -3,7 +3,6 @@ import { CustomError } from "../utils/errors/CustomError";
 import { NextFunction, Request, Response } from "express";
 import HttpStatusCode from "../utils/enums/httpStatusCode";
 
-type ErrorToken = { [key: string]: string };
 export default function authenticateToken( req: Request, res: Response, next: NextFunction ) {
   const authHeader = req.headers.authorization;
 
